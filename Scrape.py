9473440944170077c6b4bcb4d506a89998edee13
Jayant Taneja <jayanttaneja1@gmail.com>
2021-12-27 13:18:07 +0530

    renaming

--- Scrape.py
+++ Scrape.py
@@ -9,16 +9,19 @@ def clean(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", "{}".format(tweet)).split())
 
 
-def scrape_tweets(search_query, n=10,start=0, end=0):
+def scrape_tweets(search_query, max_tweets_count=10,start=0, end=0):
     
     tweets_list2 = []
     tweets_list3 = []
 
     # Using TwitterSearchScraper to scrape data and append tweets to list
-    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('{} since:{} until:{} lang:en'.format(search_query, start, end)).get_items()):
-        if i>=n:
+    for fetched_tweets_no, tweet in enumerate(sntwitter.TwitterSearchScraper('{} since:{} until:{} lang:en'.format(search_query, start, end)).get_items()):
+        
+        if fetched_tweets_no>=max_tweets_count:
             break
+        
         tweets_list2.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+        
         po, su = sentiment(tweet.content)
         tweets_list3.append([tweet.date, tweet.id, clean(tweet.content), po, su, tweet.user.username])
         
